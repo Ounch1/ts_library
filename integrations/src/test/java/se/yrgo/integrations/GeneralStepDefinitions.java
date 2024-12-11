1172d573edcb2f0f5b98f536cb76333784bf95c2
Ilja Grigorjev <tomkapisos@gmail.com>
2024-12-11 19:55:53 +0100

    Update

---
+++ integrations/src/test/java/se/yrgo/integrations/GeneralStepDefinitions.java
@@ -0,0 +1,29 @@
+public class GeneralStepDefinitions {
+    private static WebDriver driver;
+    @Before
+    public void setupWebDriver() {
+        try {
+            ChromeOptions options = new ChromeOptions();
+            driver = new RemoteWebDriver(new URL("http://localhost:4444"),
+                    options, false);
+        } catch (MalformedURLException e) {
+            fail(e);
+        }
+    }
+    @After
+    public void shutdownWebDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    public static WebDriver getDriver() {
+        return driver;
+    }
+    @Given("the user is on the start page.")
+    public void the_user_is_on_the_start_page() {
+        driver.get("http://frontend");
+        if (!"The Library".equals(driver.getTitle())) {
+            throw new IllegalStateException("Not on the start page");
+        }
+    }
+}
