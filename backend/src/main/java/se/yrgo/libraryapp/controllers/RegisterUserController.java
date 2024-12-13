d29b3e2deba0d295698828f3f54d23bbee1a41ce
Ilja Grigorjev <tomkapisos@gmail.com>
2024-12-13 20:26:27 +0100

    Sanitize controller input

--- backend/src/main/java/se/yrgo/libraryapp/controllers/RegisterUserController.java
+++ backend/src/main/java/se/yrgo/libraryapp/controllers/RegisterUserController.java
@@ -5,10 +5,12 @@ import io.jooby.annotations.GET;
 import io.jooby.annotations.POST;
 import io.jooby.annotations.Path;
 import io.jooby.annotations.QueryParam;
+import org.apache.commons.text.StringEscapeUtils;
 import se.yrgo.libraryapp.dao.UserDao;
 import se.yrgo.libraryapp.entities.forms.RegisterUserData;
 import se.yrgo.libraryapp.validators.RealName;
 import se.yrgo.libraryapp.validators.Username;
+
 
 @Path("/register")
 public class RegisterUserController {
@@ -21,10 +23,13 @@ public class RegisterUserController {
 
     @POST
     public boolean register(RegisterUserData userData) {
-        if (Username.validate(userData.getName()) && RealName.validate(userData.getRealName())) {
-            return userDao.register(userData.getName(), userData.getRealName(), userData.getPassword());
+        String sanitizedRealName = StringEscapeUtils.escapeHtml4(userData.getRealName());
+        String sanitizedName = StringEscapeUtils.escapeHtml4(userData.getName());
+
+        if (Username.validate(sanitizedName) && RealName.validate(sanitizedRealName)) {
+            return userDao.register(sanitizedName, sanitizedRealName, userData.getPassword());
         }
-        
+
         return false;
     }
 
